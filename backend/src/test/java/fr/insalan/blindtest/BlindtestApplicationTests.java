a74ca31d8b3e5735116f1b1a90749a617c840073
NyxCormo <laine.clement@outlook.fr>
2026-09-20 22:04:06 +0200

    initialize spring boot backend

---
+++ backend/src/test/java/fr/insalan/blindtest/BlindtestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insalan.blindtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
