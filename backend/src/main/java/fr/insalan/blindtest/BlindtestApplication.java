189291c0562043a81c08ae6f7aaa34f06e5b7e88
NyxCormo <laine.clement@outlook.fr>
2026-09-20 23:06:01 +0200

    Added SQLite, JPA and Flyway to the backend

--- backend/src/main/java/fr/insalan/blindtest/BlindtestApplication.java
+++ backend/src/main/java/fr/insalan/blindtest/BlindtestApplication.java
@@ -1,12 +1,18 @@
 package fr.insalan.blindtest;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlindtestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// SQLite creates .db file automatically, but not data/
+		Files.createDirectories(Path.of("data"));
 		SpringApplication.run(BlindtestApplication.class, args);
 	}
 
--- backend/src/test/java/fr/insalan/blindtest/BlindtestApplicationTests.java
+++ backend/src/test/java/fr/insalan/blindtest/BlindtestApplicationTests.java
@@ -2,8 +2,10 @@ package fr.insalan.blindtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlindtestApplicationTests {
 
 	@Test
